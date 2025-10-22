90e76b4149baec2fefe7e529490e16709c3ede69
SiNoova <alaekoutate1337@outlook.fr>
2025-10-22 08:24:20 +0100

    added some modes

--- commands/mode.cpp
+++ commands/mode.cpp
@@ -10,37 +10,18 @@ void send_numeric(client_info* client, const std::string& code,
 }
 
 // checks if the client is an operator in the specified channel
-// bool check_if_op(channel* di_channel, client_info *client)
-// {
-//     // Print all moderators
-// 	exit(0);
-//     std::cout << "Moderators in channel " << di_channel->name << ": ";
-//     for (std::vector<client_info>::const_iterator it = di_channel->moderators.begin();
-//          it != di_channel->moderators.end(); ++it)
-//     {
-//         std::cout << (it)->nickname << " ";
-//     }
-//     std::cout << std::endl;
-
-//     // Check if the given client is an operator
-//     std::vector<client_info>::const_iterator it =
-//         std::find(di_channel->moderators.begin(),
-//                   di_channel->moderators.end(),
-//                   client);
-
-//     return (it != di_channel->moderators.end());
-// }
-
-bool check_if_op(channel* di_channel, client_info* client)
+bool check_if_op(channel* di_channel, client_info *client)
 {
     for (std::vector<client_info>::const_iterator it = di_channel->moderators.begin();
          it != di_channel->moderators.end(); ++it)
     {
-        if (it->nickname == client->nickname) // simple comparison
+        if (it->nickname == client->nickname)
             return true;
     }
     return false;
 }
+
+
 
 // find the channel that has the same name in the list of channels
 channel* find_channel(const std::string &channel_to_find, std::deque<channel> &channels)
@@ -58,8 +39,9 @@ void mode(std::vector<std::string> tokens, std::deque<channel> &channels, client
 {
 	std::string channel_name;
 	channel* di_channel;
-	std::vector<char> valid_modes;
-
+	std::string modes;
+	bool add = true;
+	int args_start = 3;
 	// checks if it has valid args
 	if (tokens.size() < 3)
 		return (send_numeric(client_connected, ERR_NEEDMOREPARAMS, "MODE", "Not enough parameters\n"));
@@ -74,20 +56,63 @@ void mode(std::vector<std::string> tokens, std::deque<channel> &channels, client
 		return (send_numeric(client_connected, ERR_CHANOPRIVSNEEDED, channel_name, "You're not channel operator\n"));
 
 	// check if modes are valid
+	modes = tokens[2];
 	std::string allowed = "itkol";
 	if (tokens[2].empty() || (tokens[2][0] != '+' && tokens[2][0] != '-')) 
 		return (send_numeric(client_connected, ERR_UNKNOWNMODE, std::string(1,tokens[2][0]), "is unknown mode char to me\n"));
 
-	// push the + / -
-	valid_modes.push_back(tokens[2][0]);
-
-	// push valid modes to vec and send error for unvalid ones
-	for (int i = 1; i < tokens[2].size(); i++)
+	for (int i = 0; i < modes.size(); i++)
 	{
-		if (allowed.find(tokens[2][i]) == std::string::npos)
-			send_numeric(client_connected, ERR_UNKNOWNMODE, std::string(1,tokens[2][i]), "is unknown mode char to me\n");
-		else
-			valid_modes.push_back(tokens[2][i]);
+		if (modes[i] == '+')
+		{
+			add = true;
+			continue;
+		}
+		if (modes[i] == '-')
+		{
+			add = false;
+			continue;
+		}
+		if (modes[i] ==  'i')
+			di_channel->i = add;
+		else if (modes[i] == 't')
+			di_channel->t = add;
+		else if (modes[i] == 'k')
+		{
+			if (add)
+			{
+				if (args_start >= tokens.size())
+				{
+					send_numeric(client_connected, ERR_NEEDMOREPARAMS, "MODE", "Not enough parameters\n");
+					continue;
+				}
+				di_channel->k = add;
+				di_channel->key = tokens[args_start++];
+			}
+			else
+			{
+				di_channel->k = add;
+				di_channel->key = "";
+			}
+		}
+		else if (modes[i] == 'o')
+		{
+			if (add)
+			{
+				if (args_start >= tokens.size())
+				{
+					send_numeric(client_connected, ERR_NEEDMOREPARAMS, "MODE", "Not enough parameters\n");
+					continue;
+				}
+
+			}
+			else
+			{
+				di_channel->k = add;
+				di_channel->key = "";
+			}
+		}
+		
 	}
 
 	send(client_connected->fd, "l7waaa\n", 8, 0);
